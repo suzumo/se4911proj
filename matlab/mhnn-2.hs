9c39a9b6794e1fa2d9c589fbdd00b4f7dea26382
crs.jeong <crs.jeong@gmail.com>
2017-04-07 14:31:23 +1000

    translating fmincg

--- matlab/mhnn-2.hs
+++ matlab/mhnn-2.hs
@@ -13,10 +13,10 @@ import Data.Array.Accelerate                      as A
 import Data.Array.Accelerate.Interpreter          as I
 -- import Data.Array.Accelerate.System.Random.MWC
 
--- import MMult                                      ( Matrix, mmult )
+import MMult                                      ( Matrix, mmult )
 
 
-type Matrix a = Array DIM2 a
+-- type Matrix a = Array DIM2 a
 
 -- trying to translate coursera machine learning neural network 
 -- into accelerate or haskell version...
@@ -58,10 +58,11 @@ loadLabelA = do
     let arr = A.fromList (Z:.100) dbarr :: Vector Float
     return arr
 
-generateTheta :: Int -> Vector Float
-generateTheta n = A.fromList (Z :. n) (P.replicate n 0) :: Vector Float
---  withSystemRandom $ \gen ->
---    randomArray (uniformR (0,1)) (Z :. n)
+generateTheta :: Int -> IO (Vector Float)
+generateTheta n =
+  withSystemRandom $ \gen ->
+    randomArray (uniformR (0,1)) (Z :. n)
+
 
 {--
 -- matlab function: lrCostFunction(theta, X, y, lambda) = [J, grad]
@@ -87,12 +88,11 @@ lrCostFunction
     -> Acc (Matrix Float)               -- x
     -> Acc (Vector Float)               -- y (training .. bit?)
     -> Exp Float                        -- ??
-    -- -> Acc (Scalar Float, Matrix Float)
-    -> (Acc (Scalar Float), Acc (Vector Float))
-lrCostFunction theta xs ys lambda = (j, grad)  -- lift :: (Acc (Scalar Float), Acc (Vector Float)) -> Acc (Scalar Float, Matrix Float)
+    -> Acc (Scalar Float, Matrix Float)
+lrCostFunction theta xs ys lambda = lift (j, grad)
   where
-    grad :: Acc (Vector Float)
-    grad = use $ fromList (Z :. 0) []
+    grad :: Acc (Matrix Float)
+    grad = use $ fromList (Z :. 0 :. 0) []
 
     m :: Exp Int
     m = A.length ys
@@ -109,7 +109,7 @@ lrCostFunction theta xs ys lambda = (j, grad)  -- lift :: (Acc (Scalar Float), A
       $ A.zipWith3 (\x y t -> -y * log (sigmoid (t * x)) - (1 - y) * log (1 - sigmoid (t * x))) xs yy tt
 
     -- replicate column vector y into a matrix; where the column is replicated
-    -- 'w' times across to form the matrix:
+    -- w times across to form the matrix:
     --
     --   y1        y1 y1 y1 ...
     --   y2   ->   y2 y2 y2 ...
@@ -118,18 +118,12 @@ lrCostFunction theta xs ys lambda = (j, grad)  -- lift :: (Acc (Scalar Float), A
     yy :: Acc (Matrix Float)
     yy  = A.replicate (lift (Z :. All :. w)) ys
 
-    -- same but replicate so that every row is the same, and is 'h' rows high
-    --
-    --   t1 t2 t3  ->  t1 t2 t3 ...
-    --                 t1 t2 t3 ...
-    --                    ...
-    --
+    -- same but replicate so that every row is the same
     tt :: Acc (Matrix Float)
     tt = A.replicate (lift (Z :. h :. All)) theta
 
-
-cubicExtrapolate :: Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> (Exp Float, Exp Float, Exp Float)
-cubicExtrapolate d2 d3 f2 f3 z3 = (a, b, z2)
+cubicExtrapol :: Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float
+cubicExtrapol d2 d3 f2 f3 z3 = z2
                          where
                              a = 6*(f2 - f3)/z3 + 3*(d2 + d3)
                              b = 3*(f3 - f2) - z3*(d3 + 2*d2)
@@ -144,73 +138,6 @@ cubicFit d2 d3 f2 f3 z3 = z2
 
 quadraticFit :: Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float
 quadraticFit d3 f2 f3 z3 = z3 - (0.5*d3*z3*z3)/(d3*z3 + f2 - f3)
-
-innerWhile :: Acc (Vector Float) -> Acc (Vector Float) -> Exp Float -> Exp Float -> Exp Float -> Exp Float
- -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Acc (Matrix Float) -> Acc (Vector Float) -> Exp Float 
- -> (Acc (Vector Float), Acc (Scalar Float), Acc (Scalar Float), Exp Float, Exp Float, Exp Float)
-innerWhile theta s d1 d2 d3 f1 f2 f3 z1 z2''' z3 xs ys lambda = (theta', d2', f2', z1', z2, z3')
-    where
-        condition = (f2 > (f1 + z1 * 0.01 * d1)) || (d2 > -0.5 * d1)
-    -- if condition == True, then loop, otherwise return/break innerWhile loop
-        z2'' | (f2 > f1) = quadraticFit d3 f2 f3 z3
-             | otherwise = cubicFit d2 d3 f2 f3 z3
-        z2'  | (z2'' /= z2'') = z3/2
-             | otherwise = z2''
-        z2 = max (min z2' (0.1 * z3)) (0.9 * z3)
-        z1' = z1 + z2
-        theta' = A.zipWith (+) theta (multScalerVector z2 s)
-        (f2', df2') = lrCostFunction theta' xs ys lambda
-        d2' = A.sum (A.zipWith (*) df2' s)
-        z3' = z3 - z2;
-    -- return z2 -- TODO
-
-outerWhile :: Acc (Vector Float) -> Acc (Vector Float) -> Exp Float -> Exp Float -> Exp Float -> Exp Float
- -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Acc (Matrix Float) -> Acc (Vector Float) -> Exp Float -> Exp Float 
- -> (Acc (Vector Float), Acc(Scalar Float), Exp Float, Acc(Scalar Float), Exp Float, Exp Float, Exp Float, Exp Float, Acc (Vector Float))
-outerWhile theta s d1 d2 d3 f1 f2 f3 z1 z2 z3 xs ys lambda limit = (theta', d2', d3', f2', f3', z1', z2', z3', df2')
-    where
-        -- innerWhile here
-        (a, b, z2') = cubicExtrapolate d2 d3 f2 f3 z3
-        z2'' | ((z2 /= z2 || z2 < 0) && limit < -0.5) = z1 * 2.0
-             | ((z2 /= z2 || z2 < 0) && limit >= -0.5) = (limit - z1)/2
-             | (limit > -0.5 && z2 + z1 > limit) = (limit - z1)/2
-             | (limit < -0.5 && z2 + z1 > z1 * 3.0) = z1 * 2.0
-             | (z2 < -z3 * 0.1) = -z3 * 0.1
-             | (limit > -0.5 && z2 < (limit -z1) * 0.9) = (limit -z1) * 0.9
-             | otherwise = z2'
-        f3' = f2
-        d3' = d2
-        z3' = -z2''
-        z1' = z1 + z2''
-        theta' = A.zipWith (+) theta (multScalerVector z2'' s)
-        (f2', df2') = lrCostFunction theta' xs ys lambda
-        d2' = A.sum (A.zipWith (*) df2' s)
-    -- return z2'' -- TODO
-
-handleSuccess :: Acc (Vector Float) -> Acc (Vector Float) -> Exp Float -> Exp Float -> Exp Float -> Exp Float
- -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Exp Float -> Acc (Matrix Float) -> Acc (Vector Float) -> Acc (Vector Float) -> Exp Float -> Exp Float -> IO (Exp Float)
-handleSuccess theta s d1 d2 d3 f1 f2 f3 z1 z2 z3 xs df1 df2 lambda limit = do
-    let f1' = f2
-    -- let fX = fX' ++ [f1]
-    -- s = (df2'*df2-df1'*df2)/(df1'*df1)*s - df2;      % Polack-Ribiere direction
-    -- let s' = A.zipWith (-) (firstbit / secondbit) df2
-    --         where
-    --             firstbit = A.zipWith (-) (A.sum (A.zipWith (*) df2 df2)) (A.sum (A.zipWith (*) df1 df2))
-    --             secondbit = multScalerVector (the (A.sum (A.zipWith (*) df1 df1))) s
-    let df1' = df2
-    let df2' = df1
-    let d2' = A.sum (A.zipWith (*) df1 s)
-    -- let (s'', d2'') | (d2' > 0) = (-df1', A.sum (A.zipWith (*) s s)) -- TODO one of the s should be -s
-    --                 | otherwise = (s', d2')
-    -- let z1' = z1 * (min 100 (d1/(d2-realmin))) -- TODO find what realmin is
-    let d1' = d2'
-    return z2;
-
-multScalerVector :: Exp Float -> Acc (Vector Float) -> Acc (Vector Float)
-multScalerVector f v = A.zipWith (*) f' v
-    where
-        f' = A.replicate (lift (Any :. h)) (unit f)
-        Z :. h = unlift (shape v) :: Z :. Exp Int
 
 -- fmincg
 --     :: Acc (Vector Float)               -- initial_theta
